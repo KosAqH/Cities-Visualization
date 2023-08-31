8564b1be02e04d9cc3157f282215ac78eee91513
KosAqH <marcin_wlodarczyk@protonmail.com>
2023-08-31 10:50:31 +0200

    Fix error - all radio buttons are checked properly now

--- app/app/static/fill.js
+++ app/app/static/fill.js
@@ -2,4 +2,5 @@ function setForm(value1, value2){
     form.elements["phrase1"].value = value1;
     form.elements["phrase2"].value = value2;
     form.elements["positioning1-2"].checked = true;
+    form.elements["positioning2-2"].checked = true;
 }
